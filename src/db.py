eb6babdce01a71fcd6dd7a16813840e78cfee3a4
Alejandro Ranchal <100432239@alumnos.uc3m.es>
2026-01-13 17:17:32 +0100

    v1

--- src/db.py
+++ src/db.py
@@ -1,16 +1,12 @@
 import psycopg2
 import os
-from dotenv import load_dotenv
-
-load_dotenv()
 
 def get_conn():
     return psycopg2.connect(
-        host=os.getenv("DB_HOST"),
-        port=os.getenv("DB_PORT"),
-        database=os.getenv("DB_NAME"),
-        user=os.getenv("DB_USER"),
-        password=os.getenv("DB_PASSWORD"),
-        sslmode="require",  # Mantén la conexión segura
-        connect_timeout=10  # Timeout de 10 segundos
+        host=os.environ["DB_HOST"],
+        port=os.environ["DB_PORT"],
+        database=os.environ["DB_NAME"],
+        user=os.environ["DB_USER"],
+        password=os.environ["DB_PASSWORD"],
+        connect_timeout=15
     )
